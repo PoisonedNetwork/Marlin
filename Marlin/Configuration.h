698aede5f0a6b20c2ccead16456b2bda6e97f84a
Rubén Ortiz Córdoba <roc_360@hotmail.com>
2017-08-01 23:58:37 +0200

    Config SD

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1203,7 +1203,7 @@
  * you must uncomment the following option or it won't work.
  *
  */
-//#define SDSUPPORT
+#define SDSUPPORT
 
 /**
  * SD CARD: SPI SPEED
@@ -1220,7 +1220,7 @@
  *
  * Use CRC checks and retries on the SD communication.
  */
-//#define SD_CHECK_AND_RETRY
+#define SD_CHECK_AND_RETRY
 
 //
 // ENCODER SETTINGS
